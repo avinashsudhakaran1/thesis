17156f964afb7e35241bd2eab4af6a5bdd653a96
Avinash Sudhakaran <avinash.sudhakaran1@gmail.com>
2022-10-24 21:45:33 +0800

    removed reduntant API keys

--- import_labels.py
+++ import_labels.py
@@ -1,10 +1,10 @@
 import labelbox
 # Enter your Labelbox API key here
-LB_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbDZ4c25vYzIxbmwzMDc1aGJhbGVjb3huIiwib3JnYW5pemF0aW9uSWQiOiJjbDZ4c25vYm8xbmwyMDc1aDRubmJkMm4wIiwiYXBpS2V5SWQiOiJjbDc0b2VxY3U0dnZsMDcwa2dpbTBhZTN6Iiwic2VjcmV0IjoiYjExZjE1NTcyNzNlMTMwYjQzYzQ3ZmFhZTFlZWY5MGEiLCJpYXQiOjE2NjExNjc3ODUsImV4cCI6MjI5MjMxOTc4NX0.lCMCqLCf3EEgtnVdB2D9PlKwdPs2f-aseg99bsCvhdM"
+LB_API_KEY = ""
 # Create Labelbox client
 lb = labelbox.Client(api_key=LB_API_KEY)
 # Get project by ID
-project = lb.get_project('cl72s9m8xbtlb0738azvvgy2b')
+project = lb.get_project('')
 # Export image and text data as an annotation generator:
 labels = project.label_generator()
 
